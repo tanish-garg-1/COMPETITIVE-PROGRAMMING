af39f585ab32128bcb1c3e9dab1f1f3e22bb9de8
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-01-27 17:36:00 +0530

    Add files via upload

---
+++ CODEFORCES/Reverse Binary Strings.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		string s;
+		cin>>s;
+		int one =0,zero=0;
+		for(int i=0;i<n-1;i++){
+			if(s[i]=='0' && s[i+1]=='0') zero++;
+			if(s[i]=='1' && s[i+1]=='1') one++;
+		}
+		cout<<max(zero,one)<<endl;
+	}
+}
